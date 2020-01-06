2f9ca0a61b7dfe1cd140416b090fd5d68949eb80
Wesley Jacobs <w.jacobs.632@gmail.com>
2020-01-06 12:33:51 -0800

    added user context and imported router-dom

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import { Route, Switch } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>The Social Playlist</h1>
-    </div>
-  );
+export default class App extends Component {
+  state = { hasError: false };
+  render() {
+    return (
+      <div className="App">
+        <h1>The Social Playlist</h1>
+      </div>
+    );
+  }
 }
-
-export default App;
