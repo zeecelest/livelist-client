7c9e0cb923e40178e359baa41227509bbee299b8
Zee Celest <zeecelest@gmail.com>
2020-01-07 12:53:38 -0500

    adding to description on landing page

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -17,9 +17,14 @@ class LandingPage extends React.Component {
            <section>
                <p>Social Playlist makes it easy for you to find the perfect outing. 
                    Whether a nite on the town with friends, date night with your love,
-                   or a day with the family, Social Playlist has what you need!
+                   or a day with the family!  Social Playlist solves the problem of not 
+                   knowing what to do with providing customized playlists created for users 
+                   by users.
                </p>
-               <p>See what </p>
+               <p>See what social events people enjoy in their areas.  You can like other
+                   users playlist.  You can also follow the user with your favorite playlists.
+                   Develop your own playlist and share with your follows.  
+               </p>
                <p>placeholder for playlist example</p>
            </section>
             </>
