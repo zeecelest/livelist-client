ddaf5d42fccb084dd7c8baa2562a2b0409203672
Wesley Jacobs <w.jacobs.632@gmail.com>
2020-01-08 06:59:41 -0800

    added context

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App/App";
 import { BrowserRouter } from "react-router-dom";
+import { PlayListContext } from "./contexts/PlayListContext";
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <PlayListContext>
+      <App />
+    </PlayListContext>
   </BrowserRouter>,
   document.getElementById("root")
 );
