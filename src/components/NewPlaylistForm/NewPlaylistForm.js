18c838973a4eb6b5b2a36e1efc77fc74bb0a8d48
Wesley Jacobs <w.jacobs.632@gmail.com>
2020-01-09 09:16:56 -0800

    form is behaving correctly

--- src/components/NewPlaylistForm/NewPlaylistForm.js
+++ src/components/NewPlaylistForm/NewPlaylistForm.js
@@ -8,76 +8,81 @@ class NewPlaylistForm extends Component {
   static defaultProps = {
     onPlaylistCreation: () => {}
   };
-
-  state = { error: null };
+  stateAbr = [
+    "AL",
+    "AK",
+    "AS",
+    "AZ",
+    "AR",
+    "CA",
+    "CO",
+    "CT",
+    "DE",
+    "DC",
+    "FM",
+    "FL",
+    "GA",
+    "GU",
+    "HI",
+    "ID",
+    "IL",
+    "IN",
+    "IA",
+    "KS",
+    "KY",
+    "LA",
+    "ME",
+    "MH",
+    "MD",
+    "MA",
+    "MI",
+    "MN",
+    "MS",
+    "MO",
+    "MT",
+    "NE",
+    "NV",
+    "NH",
+    "NJ",
+    "NM",
+    "NY",
+    "NC",
+    "ND",
+    "MP",
+    "OH",
+    "OK",
+    "OR",
+    "PW",
+    "PA",
+    "PR",
+    "RI",
+    "SC",
+    "SD",
+    "TN",
+    "TX",
+    "UT",
+    "VT",
+    "VI",
+    "VA",
+    "WA",
+    "WV",
+    "WI",
+    "WY"
+  ];
+  state = {
+    error: null,
+    name: "",
+    city: " ",
+    state: "",
+    tags: " ",
+    is_public: true
+  };
 
   firstInput = React.createRef();
   renderOptions = () => {
-    let stateAbr = [
-      "AL",
-      "AK",
-      "AS",
-      "AZ",
-      "AR",
-      "CA",
-      "CO",
-      "CT",
-      "DE",
-      "DC",
-      "FM",
-      "FL",
-      "GA",
-      "GU",
-      "HI",
-      "ID",
-      "IL",
-      "IN",
-      "IA",
-      "KS",
-      "KY",
-      "LA",
-      "ME",
-      "MH",
-      "MD",
-      "MA",
-      "MI",
-      "MN",
-      "MS",
-      "MO",
-      "MT",
-      "NE",
-      "NV",
-      "NH",
-      "NJ",
-      "NM",
-      "NY",
-      "NC",
-      "ND",
-      "MP",
-      "OH",
-      "OK",
-      "OR",
-      "PW",
-      "PA",
-      "PR",
-      "RI",
-      "SC",
-      "SD",
-      "TN",
-      "TX",
-      "UT",
-      "VT",
-      "VI",
-      "VA",
-      "WA",
-      "WV",
-      "WI",
-      "WY"
-    ];
-
-    return stateAbr.map(state => {
+    return this.stateAbr.map(state => {
       return (
-        <option key={state} value={state}>
+        <option key={state} value={state} onChange={this.handleChange}>
           {state}
         </option>
       );
@@ -90,26 +95,42 @@ class NewPlaylistForm extends Component {
     if (state.value === "") {
       return this.setState({ error: "Please select a state." });
     }
-
+    console.log("name", name.value);
+    console.log("city", city.value);
+    console.log("state", state.value);
+    console.log("tags", tags.value);
+    console.log("is_public", this.state.is_public);
+    console.log(this.state);
     // TODO needs to be converted to the API call for posting a new Playlist
-    AuthApiService.postPlaylist({
-      name: name.value,
-      city: city.value,
-      state: state.value,
-      tags: tags.value,
-      is_public: is_public.value
-    })
-      .then(playlist => {
-        name.value = "";
-        state.value = "";
-        city.value = "";
-        tags.value = "";
-        is_public.value = "";
-        this.props.onPlaylistCreation();
-      })
-      .catch(res => {
-        this.setState({ error: res.error });
-      });
+    // AuthApiService.postPlaylist({
+    //   name: name.value,
+    //   city: city.value,
+    //   state: state.value,
+    //   tags: tags.value,
+    //   is_public: is_public.value
+    // })
+    //   .then(playlist => {
+    //     name.value = "";
+    //     state.value = "";
+    //     city.value = "";
+    //     tags.value = "";
+    //     is_public.value = "";
+    //     this.props.onPlaylistCreation();
+    //   })
+    //   .catch(res => {
+    //     this.setState({ error: res.error });
+    //   });
+  };
+
+  handleChange = ev => {
+    const target = ev.target;
+    const value = (target.type === "checkbox" ? !target.checked : target.value);
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+    console.log(this.state);
   };
 
   componentDidMount() {
@@ -130,6 +151,8 @@ class NewPlaylistForm extends Component {
             ref={this.firstInput}
             id="newPlaylist-name-input"
             name="name"
+            value={this.state.value}
+            onChange={this.handleChange}
             required
           />
         </div>
@@ -138,15 +161,21 @@ class NewPlaylistForm extends Component {
             City
             <Required />
           </Label>
-          <Input id="newPlaylist-city-input" name="city" required />
+          <Input
+            id="newPlaylist-city-input"
+            name="city"
+            value={this.state.value}
+            onChange={this.handleChange}
+            required
+          />
         </div>
         <div>
           <Label htmlFor="registration-state-input">
             State
             <Required />
           </Label>
-          <select className="state" name="state">
-            <option key="none" defaultValue=""></option>
+          <select className="state" name="state" onChange={this.handleChange}>
+            <option key="none" defaultValue={this.state.value}></option>
             {this.renderOptions()}
           </select>
         </div>
@@ -155,16 +184,26 @@ class NewPlaylistForm extends Component {
             Tags for your list
             <Required />
           </Label>
-          <Input id="newPlaylist-tags-input" name="tags" />
+          <Input
+            id="newPlaylist-tags-input"
+            name="tags"
+            value={this.state.value}
+            onChange={this.handleChange}
+            placeholder="#datenight #hotnewspots"
+          />
         </div>
         <div>
           <Label htmlFor="newPlaylist-public-input">
-            Would you like allow others to see your playlist?
+            Would you like to make this list private?
             <Required />
           </Label>
-          <input type="select" name="is_public"></input>
+          <input
+            type="checkbox"
+            name="is_public"
+            value={this.state.value}
+            onChange={this.handleChange}
+          ></input>
         </div>
-
         <footer className="signupBtnLink">
           <Button type="submit">Submit</Button> <br />{" "}
         </footer>
