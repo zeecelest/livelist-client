6ecbf0ff61b8aa49ea99c1e36644bd8588406d7b
julio hernandez <hernandezjulio58@ymail.com>
2020-01-13 14:52:09 -0800

    wlogin spec

--- cypress/integration/day-1.1-registration.spec.js
+++ cypress/integration/day-1.1-registration.spec.js
@@ -12,45 +12,48 @@
 */
 describe(`User story: Register an account`, function() {
   it('on first load, directs me to the registration page', () => {
-    cy.visit('/')
-    cy.url()
-      .should('eq', `${Cypress.config().baseUrl}/register`)
+    cy.visit('/');
+    cy.url().should('eq', `${Cypress.config().baseUrl}/register`);
 
-    cy.get('main section').within($section => {
-      cy.get('h2')
-        .should('have.text', 'Sign up')
-    })
-  })
+    cy.get('main section').within(($section) => {
+      cy.get('h2').should('have.text', 'Sign up');
+    });
+  });
 
   it(`displays the name, username and password fields`, () => {
-    cy.visit('/register')
+    cy.visit('/register');
 
     cy.get('section form').within(() => {
-      cy.get('label[for=registration-name-input]')
-        .should('have.text', 'Enter your name*')
+      cy.get('label[for=registration-name-input]').should(
+        'have.text',
+        'Enter your name*'
+      );
       cy.get('input#registration-name-input')
         .should('have.attr', 'type', 'text')
-        .and('have.attr', 'required', 'required')
+        .and('have.attr', 'required', 'required');
 
-      cy.get('label[for=registration-username-input]')
-        .should('have.text', 'Choose a username*')
+      cy.get('label[for=registration-username-input]').should(
+        'have.text',
+        'Choose a username*'
+      );
       cy.get('input#registration-username-input')
         .should('have.attr', 'type', 'text')
-        .and('have.attr', 'required', 'required')
+        .and('have.attr', 'required', 'required');
 
-      cy.get('label[for=registration-password-input]')
-        .should('have.text', 'Choose a password*')
+      cy.get('label[for=registration-password-input]').should(
+        'have.text',
+        'Choose a password*'
+      );
       cy.get('input#registration-password-input')
         .should('have.attr', 'type', 'password')
-        .and('have.attr', 'required', 'required')
+        .and('have.attr', 'required', 'required');
 
-      cy.get('button[type=submit]')
-        .should('have.text', 'Sign up')
-    })
-  })
+      cy.get('button[type=submit]').should('have.text', 'Sign up');
+    });
+  });
 
   context(`Given invalid information`, () => {
-    const serverError = 'Some error from the server'
+    const serverError = 'Some error from the server';
 
     beforeEach(() => {
       cy.server()
@@ -61,34 +64,35 @@ describe(`User story: Register an account`, function() {
           status: 400,
           response: {
             error: serverError
-          },
+          }
         })
-        .as('postRegister')
-    })
+        .as('postRegister');
+    });
 
     it(`displays error from POSTS /api/users`, () => {
       const newUser = {
         name: 'Test name',
         username: 'invalid-username',
         password: 'invalid-password',
-      }
-      cy.visit('/register')
+        city: 'Los Angeles',
+        state: 'CA'
+      };
+      cy.visit('/register');
 
-      cy.get('main form').within($form => {
-        cy.get('#registration-name-input')
-          .type(newUser.name)
-        cy.get('#registration-username-input')
-          .type(newUser.username)
-        cy.get('#registration-password-input')
-          .type(newUser.password)
-        cy.root().submit()
+      cy.get('main form').within(($form) => {
+        cy.get('#registration-name-input').type(newUser.name);
+        cy.get('#registration-username-input').type(newUser.username);
+        cy.get('#registration-password-input').type(newUser.password);
+        cy.get('#registration-locationState-input').type(newUser.state);
+        cy.get('#registration-locationCity-input').type(newUser.city);
+        cy.root().submit();
 
         cy.wait('@postRegister')
           .get('[role=alert]')
-          .should('have.text', serverError)
-      })
-    })
-  })
+          .should('have.text', serverError);
+      });
+    });
+  });
 
   context(`Given valid information`, () => {
     beforeEach(() => {
@@ -101,32 +105,31 @@ describe(`User story: Register an account`, function() {
           response: {
             id: 123,
             name: 'test name',
-            username: 'test username',
-          },
+            username: 'test username'
+          }
         })
-        .as('postRegister')
-    })
+        .as('postRegister');
+    });
 
     it(`redirects to /login`, () => {
       const newUser = {
         name: 'Test name',
         username: 'test-username',
-        password: 'test-password',
-      }
-      cy.visit('/register')
+        password: 'test-password'
+      };
+      cy.visit('/register');
 
-      cy.get('section form').within($form => {
-        cy.get('#registration-name-input')
-          .type(newUser.name)
-        cy.get('#registration-username-input')
-          .type(newUser.username)
-        cy.get('#registration-password-input')
-          .type(newUser.password)
-        cy.root().submit()
+      cy.get('section form').within(($form) => {
+        cy.get('#registration-name-input').type(newUser.name);
+        cy.get('#registration-username-input').type(newUser.username);
+        cy.get('#registration-password-input').type(newUser.password);
+        cy.get('#registration-locationState-input').type(newUser.state);
+        cy.get('#registration-locationCity-input').type(newUser.city);
+        cy.root().submit();
         cy.wait('@postRegister')
           .url()
-          .should('eq', `${Cypress.config().baseUrl}/login`)
-      })
-    })
-  })
-})
+          .should('eq', `${Cypress.config().baseUrl}/login`);
+      });
+    });
+  });
+});
