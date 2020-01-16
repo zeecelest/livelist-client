446a6542e80428a95d52d10a4e5f917862629308
Wesley Jacobs <w.jacobs.632@gmail.com>
2020-01-16 14:29:24 -0800

    added the hotIn component

---
+++ src/components/HotInComponent/HotIn.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import ScrollContainer from 'react-indiana-drag-scroll';
+import PlayListContext from '../../contexts/PlayListContext';
+
+export class HotIn extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default HotIn
