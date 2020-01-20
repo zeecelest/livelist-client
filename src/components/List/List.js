13e49e72c696aeb613e8de416f0a06c33f61806e
Wesley Jacobs <w.jacobs.632@gmail.com>
2020-01-20 08:40:06 -0800

    added userList to List component

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,33 +1,80 @@
 import React, { Component } from "react";
-import LikeButton from '../LikeButton/likeButton';
-import './List.css'
-
+import LikeButton from "../LikeButton/likeButton";
+import "./List.css";
+import {Link} from 'react-router-dom'
+import { Icon } from '@iconify/react';
+import timesCircle from '@iconify/icons-fa-regular/times-circle';
+import editIcon from '@iconify/icons-fa-regular/edit';
 
 export class List extends Component {
   render() {
-    if(this.props.liked){
+    if(this.props.filtered){
       return(
-        <div  
-        id={this.props.id} 
-        className={this.props.className}>
-        <h5 className="list-name">{this.props.name}</h5>
-        <LikeButton 
-          id={this.props.id}
-          likes={this.props.likes}
-          liked={this.props.liked}
-          handleLikeButton={this.props.handleLikeButton}
-          />
-    </div>
+        <div key={Math.random()} className="listItem filtered">
+        <Link to={`/list/${this.props.id}`}>
+          <h4 className="filteredListName">{this.props.name}</h4>
+        </Link>
+        <p className="filteredListTag">{this.props.tags}</p>
+      </div>
       )
-  } else {
-    return (
-      <div  
-        id={this.props.id} 
-        className={`listItem ${this.props.addClass}`}>
-        <h5 className="list-name">{this.props.name}</h5>
-    </div>
-    );
-  }
+    }
+    else if (this.props.user_id) {
+ return (     
+      <div  className="listItem users">
+        <p className="userListTitle">
+          <Link to={`/list/${this.props.user_id}`} className="userListTitle">
+            {this.props.name}
+          </Link>
+        </p>
+
+        <div className="userlist-buttons">
+          <button className="userlist-btn">
+            <Link
+              to={{
+                pathname: `/updateList/${this.props.user_id}`,
+                props: {
+                  userList: this.props.userList
+                }
+              }}
+            >
+              <Icon icon={editIcon} className="editIcon" />
+            </Link>
+          </button>
+          {this.props.checkLength === 0 && this.props.user_id ? (
+            <button
+              className="userlist-btn"
+              onClick={() => this.props.handleDeletePlaylist(this.props.user_id)}
+            >
+              <Icon icon={timesCircle} className="timesIcon" />
+            </button>
+          ) : (
+            <button className="userlist-btn" disabled={true}>
+              <Icon icon={timesCircle} className="timesIcon" />
+            </button>
+          )}
+        </div>
+      </div>
+      )
+    }
+    else if (this.props.liked) {
+      return (
+        <div id={this.props.id} className={this.props.className}>
+          <h5 className="list-name">{this.props.name}</h5>
+          <LikeButton
+            id={this.props.id}
+            likes={this.props.likes}
+            liked={this.props.liked}
+            handleLikeButton={this.props.handleLikeButton}
+          />
+        </div>
+      );
+    } else {
+      return (
+        <div id={this.props.id} className={`listItem ${this.props.addClass}`}>
+          <h5 className="list-name">{this.props.name}</h5>
+        </div>
+      );
+    }
   }
 }
 
