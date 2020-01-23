601dafc9cd2ba09e68ed98e70d09b3b814582d07
Wesley Jacobs <w.jacobs.632@gmail.com>
2020-01-23 12:25:22 -0800

    working on styling setup

---
+++ src/components/Form/theme.js
@@ -0,0 +1,24 @@
+﻿import React from 'react';
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider'
+import getMuiTheme from '@material-ui/styles/getThemeProps/getThemeProps'
+
+import NewPlaylistFrom from '../NewPlaylistForm/NewPlaylistForm'
+// {
+//     "palette": {
+//         "accent1Color": "#ec407a"
+//     }
+// }
+
+const muiTheme = getMuiTheme({
+    palette: {
+        "accent1Color": "#ec407a"
+    },
+  });
+
+  const Main = () => (
+    <MuiThemeProvider muiTheme={muiTheme}>
+      <NewPlaylistFrom />
+    </MuiThemeProvider>
+  );
+
+  export default Main;
--- src/routes/NewPlaylistRoute/NewPlaylistRoute.js
+++ src/routes/NewPlaylistRoute/NewPlaylistRoute.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import NewPlaylistForm from '../../components/NewPlaylistForm/NewPlaylistForm';
+import { ThemeProvider as MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+
 
 class RegistrationRoute extends Component {
   static defaultProps = {
@@ -14,11 +16,19 @@ class RegistrationRoute extends Component {
     history.push('/dashboard');
   };
 
+ theme = createMuiTheme({
+  palette: {
+      accent1Color: '#ec407a'
+  },
+});
+
   render() {
     return (
       <section>
         <h2 className="signUpFormTitle">New Playlist</h2>
-        <NewPlaylistForm onPlaylistCreation={this.handlePlaylistCreation} />
+        <MuiThemeProvider theme={this.theme}>
+          <NewPlaylistForm onPlaylistCreation={this.handlePlaylistCreation} />
+        </MuiThemeProvider>
       </section>
     );
   }
